92db986283a0325af5f12934b6b15b7c64619cf8
samirakia <ali@alis-MacBook-Air.local>
2018-04-19 21:52:04 -0700

    ruby task

---
+++ task1.rb
@@ -0,0 +1,22 @@
+class Task
+
+      def initilize
+         @task_one = "uncompleted"
+end
+
+     def task_one
+      "complete the form"
+         @task_one
+
+end
+     def task_one = new_task_one
+        @task_one = new_task_one
+end
+end
+
+ class TaskList
+   "please complete your tasks"
+     @task1
+     @task2
+     @task3
+end     
